b52af15f5066f4de5be603c2a34d54611a54c5a9
Ujjwal Kumar <31813384+Ujjwal0501@users.noreply.github.com>
2018-02-06 14:14:30 +0530

    ByUjjwal (060220181412)

---
+++ app/src/main/java/kshitij/com/acion/app/src/main/java/kshitij/com/acion/RecipeDisplay.java
@@ -0,0 +1,19 @@
+package kshitij.com.acion;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Ujjwal on 06-02-2018.
+ */
+
+public class RecipeDisplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("");
+        setContentView(R.layout.list_result);
+    }
+}
+
