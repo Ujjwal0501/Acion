4fc38920c72a145f031a121be655e06cb03bb7ce
Ujjwal Kumar <31813384+Ujjwal0501@users.noreply.github.com>
2018-02-06 12:19:38 +0530

    ByUjjwal (060220181216)

---
+++ app/src/main/java/kshitij/com/acion/RandomResult.java
@@ -0,0 +1,20 @@
+package kshitij.com.acion;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Ujjwal on 06-02-2018.
+ */
+
+public class RandomResult extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("Todays' Special");
+        setContentView(R.layout.list_result);
+
+    }
+
+}
